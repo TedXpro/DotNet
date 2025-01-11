88cfa6e7b22c38cc2ccf2abcbca13379737b23e9
TedXpro <johannes.woldeyes@gmail.com>
2025-01-11 12:48:37 +0300

    minor changes

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dca149e47b20103896f3246887490aeb6117a1ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bdf99fcece8b877a6d5a641588d62add75e4207")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
