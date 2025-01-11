f4bfbfc69086695e77049b6c7386123735c8be31
TedXpro <johannes.woldeyes@gmail.com>
2025-01-11 12:48:59 +0300

    made few changes to the delegate and events

--- NotificationServices/obj/Debug/net8.0/NotificationServices.AssemblyInfo.cs
+++ NotificationServices/obj/Debug/net8.0/NotificationServices.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotificationServices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dca149e47b20103896f3246887490aeb6117a1ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bdf99fcece8b877a6d5a641588d62add75e4207")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotificationServices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotificationServices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
